12c8a4b131e20aceb44b1309a3a4e1f6118cf258
Radomír Černoch <radomir.cernoch@gmail.com>
2010-09-19 20:06:00 +0200

    Download of files+directories capability added.

---
+++ srv/www/htdocs/api/get.php
@@ -0,0 +1,57 @@
+<?php
+header('Cache-Control: no-cache, must-revalidate');
+
+$path = $_SERVER["PATH_INFO"];
+if ($path == "") {
+	header("x", true, 415);
+	header('Content-type: text/plain');
+	echo "No path specified.\nUsage: api/get.php/path_to_your_file";
+	die;
+}
+
+$command = "sudo or-dir \"".($path)."\"";
+exec($command, $results, $retval);
+
+if ($retval != 0) {
+	header("x", true, 500);
+	header('Content-type: text/plain');
+	echo implode("\n", $results);
+	die;
+}
+
+if (sizeof($results) == 0) {
+	header("x", true, 404);
+	header('Content-type: text/plain');
+	echo "File '$path' not found!";
+	die;
+}
+
+if (sizeof($results) > 1) {
+	header("x", true, 500);
+	header('Content-type: text/plain');
+	echo "More than 1 file matches the request!";
+	die;
+}
+
+
+$tmp = explode("\t", $results[0], 6);
+$file = array(
+	"type" => $tmp[0],
+	"flag" => $tmp[1],
+	"size" => $tmp[2],
+	"mime" => $tmp[3],
+	"char" => $tmp[4],
+	"name" => $tmp[5]);
+
+header("Pragma: public");
+header("Expires: 0");
+
+header("Content-Transfer-Encoding: binary");
+header("Content-Type: {$file["mime"]}"); 
+header("Content-Length: {$file["size"]}");
+header("Content-Disposition: inline; filename=\"".urlencode($file["name"])."\"");
+passthru("sudo cat ".escapeshellarg("/mnt/".$path));
+
+//header('Content-type: application/json');
+//echo json_encode($data);
+?>
---
+++ srv/www/htdocs/api/pack.php
@@ -0,0 +1,38 @@
+<?php
+header("Expires: 0");
+header("Pragma: public");
+header("Cache-Control: no-cache, must-revalidate");
+
+$path = $_SERVER["PATH_INFO"];
+$part = explode("/", $path);
+while (sizeof($part) > 0 && $part[0] == "") array_shift($part);
+while (sizeof($part) > 0 && $part[sizeof($part)-1] == "") array_pop($part);
+
+if (sizeof($part) < 2) {
+	header("x", true, 415);
+	header('Content-type: text/plain');
+	echo "No path specified.\nUsage: api/pack.php/format/path";
+	die;
+}
+
+$type = $part[0]; array_shift($part);
+$file = $part[sizeof($part)-1];
+$path = implode("/",$part); unset($part); 
+
+if ($type == "tgz") {
+	header("Content-Type: application/x-gtar");
+	header("Content-Disposition: attachment; filename=\"".urlencode($file.".tar.gz")."\"");
+} else if ($type == "zip") {
+	header("Content-Type: application/zip");
+	header("Content-Disposition: attachment; filename=\"".urlencode($file.".zip")."\"");
+
+} else {
+	header("x", true, 415);
+	header('Content-type: text/plain');
+	echo "Unsupported archive format: '$type'.\nAllowed values are 'zip' and 'tgz'.";
+	die;	
+}
+		
+header("Content-Transfer-Encoding: binary");
+passthru("sudo or-pack ".escapeshellarg($type)." ".escapeshellarg($path));
+?>
